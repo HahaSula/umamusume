9d131ef502e4abebacd8597f43827dea10b3e5de
HahaSula <pups0506@gamil.com>
2021-09-05 16:05:56 +0800

    1. speed up reverse query

--- src/component/relation/queryP.tsx
+++ src/component/relation/queryP.tsx
@@ -25,15 +25,12 @@ interface HorseRow {
   image: string;
 }
 interface HorseGreatRow {
-  id1:string
-  image1 : string;
-  relation1: number;
-  id2:string
-  image2 : string;
-  relation2: number;
-  id3:string
-  image3 : string;
-  relation3: number;
+  parent:string
+  grandMother:string
+  grandFather:string
+  tagetOneRelation: number;
+  tagetTwoRelation: number;
+  tagetThreeRelation: number;
   relationTotal: number;
 }
 interface HorseAllRela{
@@ -89,16 +86,25 @@ class ReverseQuery extends Component<IProps, IState> {
   // eslint-disable-next-line react/sort-comp
   selectTargetOne = (event: any) => {
     const { value } = event.target;
-    this.setState({ targetOne: value });
+    let State = {...this.state};
+    State.targetOne = value;
+    this.buildRelationArray(State);
+    this.setState(state =>({ targetOne: value }));
   };
 
   selectTargetTwo = (event: any) => {
     const { value } = event.target;
+    let State = {...this.state};
+    State.targetTwo = value;
+    this.buildRelationArray(State);
     this.setState({ targetTwo: value });
   };
 
   selectTargetThree = (event: any) => {
     const { value } = event.target;
+    let State = {...this.state};
+    State.targetThree = value;
+    this.buildRelationArray(State);
     this.setState({ targetThree: value });
   };
 
@@ -116,42 +122,59 @@ class ReverseQuery extends Component<IProps, IState> {
 
   selectParentOne = (event: any) => {
     const { value } = event.target;
+    let State = {...this.state};
+    State.parentOne = value;
+    this.buildRelationArray(State);
     this.setState({ parentOne: value });
   };
 
   selectParentTwo = (event: any) => {
     const { value } = event.target;
+    let State = {...this.state};
+    State.parentTwo = value;
+    this.buildRelationArray(State);
     this.setState({ parentTwo: value });
   };
 
   selectGrandParentOne = (event: any) => {
     const { value } = event.target;
+    let State = {...this.state};
+    State.grandParentOne = value;
+    this.buildRelationArray(State);
     this.setState({ grandParentOne: value });
   };
 
   selectGrandParentTwo = (event: any) => {
     const { value } = event.target;
+    let State = {...this.state};
+    State.grandParentTwo = value;
+    this.buildRelationArray(State);
     this.setState({ grandParentTwo: value });
   };
 
   selectGrandParentThree = (event: any) => {
     const { value } = event.target;
+    let State = {...this.state};
+    State.grandParentThree = value;
+    this.buildRelationArray(State);
     this.setState({ grandParentThree: value });
   };
 
   selectGrandParentFour = (event: any) => {
     const { value } = event.target;
+    let State = {...this.state};
+    State.grandParentFour = value;
+    this.buildRelationArray(State);
     this.setState({ grandParentFour: value });
   };
 
   horses: string[];
-
-  hourseRelas: { [key: string]: number };
+  relationArray : Horsecompare[] = [];
+  
 
   constructor(props: IProps) {
     super(props);
     this.horses = Object.keys(characters);
-    this.hourseRelas = {};
     this.state = {
       targetOne: '1001',
       targetTwo: '1002',
@@ -166,7 +189,9 @@ class ReverseQuery extends Component<IProps, IState> {
       targetTwoMin: 0,
       targetThreeMin: 0,
     };
+    this.buildRelationArray(this.state);
   }
+  
 
   calculateRelation(id1: string | undefined, id2: string): number {
     if (id1 === id2 || id1 === undefined) {
@@ -184,6 +209,13 @@ class ReverseQuery extends Component<IProps, IState> {
     const targetRelations: string[] = _.intersection(relationMembers[id1], relationMembers[id2], relationMembers[id3]);
     const hourseRela = _.reduce(targetRelations, (sum: number, id: string) => sum + parseInt(relations[id], 10), 0);
     return hourseRela;
+  }
+  calculateGrandSRelation(target: string|undefined, p1: string, gp1: string, gp2: string): number {
+    if (target === p1 || target === undefined || p1 === gp1 || p1 === gp2 || gp1 === gp2) {
+      return 0;
+    }
+    
+    return this.calculateRelation(target,p1) + this.calculateGrandRelation(target, p1, gp1) + this.calculateGrandRelation(target, p1, gp2);
   }
 
   calculatePairRelation(target: string | undefined, p1: string, p2: string, gp1: string, gp2: string, gp3: string, gp4: string) {
@@ -216,11 +248,78 @@ class ReverseQuery extends Component<IProps, IState> {
     }
     return hourseList;
   }
+  buildParentArray(targetOne:string, targetTwo:string, targetThree:string, parentList : string[], gFatherList : string[] , gMotherList : string[]){
+    let parentArray:HorseGreatRow[] = [];
 
-  buildRelationArray() {
-    const relationArray : Horsecompare[] = [];
 
-    const targetList : string[] = [];
+    parentList.forEach((parent)=>{
+      gFatherList.forEach((grandFather)=>{
+        gMotherList.forEach((grandMother)=>{
+
+          if(parent !==grandFather || parent !==grandMother || grandFather !== grandMother){
+            const relation1 = this.calculateGrandSRelation(targetOne,parent,grandFather,grandMother);
+            const relation2 = this.calculateGrandSRelation(targetTwo,parent,grandFather,grandMother);
+            const relation3 = this.calculateGrandSRelation(targetThree,parent,grandFather,grandMother);
+            const relationTotal = relation1 + relation2 + relation3;
+            let IsSkyline = true;
+            for(let index = 0; index < parentArray.length; index++){
+              if(relation1 < parentArray[index].tagetOneRelation && 
+                 relation2 < parentArray[index].tagetTwoRelation && 
+                 relation3 < parentArray[index].tagetThreeRelation && 
+                 relationTotal < parentArray[index].relationTotal ){
+                  IsSkyline = false;
+                  break;
+              }
+            }
+            if(IsSkyline){
+              parentArray.push({
+                parent:parent,
+                grandMother:grandMother,
+                grandFather:grandFather,
+                tagetOneRelation: relation1,
+                tagetTwoRelation: relation2,
+                tagetThreeRelation: relation3,
+                relationTotal: relationTotal,
+              })
+            }
+            
+          }
+        })
+      })
+      for(let index = 0; index < parentArray.length-1;index++){
+        for(let index2 = parentArray.length - 1; index2 > index;index2--){
+          if(parentArray[index2].tagetOneRelation < parentArray[index].tagetOneRelation && 
+            parentArray[index2].tagetOneRelation < parentArray[index].tagetTwoRelation && 
+            parentArray[index2].tagetOneRelation < parentArray[index].tagetThreeRelation && 
+            parentArray[index2].tagetOneRelation < parentArray[index].relationTotal ){
+              parentArray.splice(index2,1)
+              
+          }
+        }
+      }
+    })
+    return parentArray;
+  }
+  
+  buildRelationArray(input:IState) {
+    const  relationArray : Horsecompare[] = [];
+    
+    const targetOne = input.targetOne;
+    const targetTwo = input.targetTwo;
+    const targetThree = input.targetThree;
+
+    const parentOne = input.parentOne;
+    const parentTwo = input.parentTwo;
+
+    const grandParentOne = input.grandParentOne;
+    const grandparentTwo = input.grandParentTwo;
+    const grandParentThree = input.grandParentThree;
+    const grandParentFour = input.grandParentFour;
+
+
+    let t0 = performance.now();
+    let t1 = performance.now();
+
     let p1List : string[];
     let p2List : string[];
     let gp1List : string[];
@@ -228,57 +327,84 @@ class ReverseQuery extends Component<IProps, IState> {
     let gp3List : string[];
     let gp4List : string[];
 
-    targetList.push(this.state.targetOne);
-    targetList.push(this.state.targetTwo);
-    targetList.push(this.state.targetThree);
-
-    p1List = this.makeHourseList(this.state.parentOne);
-    p2List = this.makeHourseList(this.state.parentTwo);
-    gp1List = this.makeHourseList(this.state.grandParentOne);
-    gp2List = this.makeHourseList(this.state.grandParentTwo);
-    gp3List = this.makeHourseList(this.state.grandParentThree);
-    gp4List = this.makeHourseList(this.state.grandParentFour);
+    p1List = this.makeHourseList(parentOne);
+    p2List = this.makeHourseList(parentTwo);
+    gp1List = this.makeHourseList(grandParentOne);
+    gp2List = this.makeHourseList(grandparentTwo);
+    gp3List = this.makeHourseList(grandParentThree);
+    gp4List = this.makeHourseList(grandParentFour);
     if (JSON.stringify(p1List) == JSON.stringify(p2List)) {
       console.log("yes");
     }
+    
 
-    for (const p1 of p1List) {
-      for (const p2 of p2List) {
-        for (const gp1 of gp1List) {
-          for (const gp2 of gp2List) {
-            for (const gp3 of gp3List) {
-              for (const gp4 of gp4List) {
-                if (gp3 !== gp4) {
-                  const targetOneRelation:number = this.calculatePairRelation(targetList[0], p1, p2, gp1, gp2, gp3, gp4);
-                  const targetTwoRelation = this.calculatePairRelation(targetList[1], p1, p2, gp1, gp2, gp3, gp4);
-                  const targetThreeRelation:number = this.calculatePairRelation(targetList[2], p1, p2, gp1, gp2, gp3, gp4);
-                  if (targetOneRelation !== 0 || targetTwoRelation !== 0 || targetThreeRelation !== 0) {
-                    relationArray.push({
-                      targetOne: targetList[0],
-                      targetTwo: targetList[1],
-                      targetThree: targetList[2],
-                      parentOne: p1,
-                      parentTwo: p2,
-                      grandParentOne: gp1,
-                      grandParentTwo: gp2,
-                      grandParentThree: gp3,
-                      grandParentFour: gp4,
-                      targetOneRela: targetOneRelation,
-                      targetTwoRela: targetTwoRelation,
-                      targetThreeRela: targetThreeRelation,
-                      sum: targetOneRelation + targetTwoRelation + targetThreeRelation,
-                    });
-                  }
-                }
-              }
-            }
-          }
-        }
+    let Father:HorseGreatRow[] = [];
+    let Mother:HorseGreatRow[] = [];
+    t1 = performance.now();
+    t0 = performance.now();
+    
+    Father = this.buildParentArray(targetOne, targetTwo, targetThree, p1List,gp1List,gp2List);
+    t1 = performance.now();
+    console.log("Father time " + (t1 - t0) + " milliseconds. father leng = " + Father.length);
+    t0 = performance.now();
+
+    Mother = this.buildParentArray(targetOne, targetTwo, targetThree,p2List,gp3List,gp4List);
+
+    t1 = performance.now();
+    console.log("Mother time " + (t1 - t0) + " milliseconds. mother leng = " + Mother.length);
+    t0 = performance.now();
+    let cal = (relaone:number,relatwo:number,relathree:number)=>{
+      if(relaone===0 || relatwo===0|| relathree===0)
+      {
+        return 0;
       }
-    }
-    console.log("123");
-    console.log(relationArray);
-    return relationArray;
+      return relaone + relatwo + relathree;
+    };
+
+    Father.forEach((father)=>{
+      Mother.forEach((mother)=>{
+        if(father.parent !== mother.parent){
+          const targetOneRela = cal(father.tagetOneRelation, mother.tagetOneRelation, this.calculateRelation(father.parent,mother.parent));
+          const targetTwoRela = cal(father.tagetTwoRelation, mother.tagetTwoRelation, this.calculateRelation(father.parent,mother.parent));
+          const targetThreeRela = cal(father.tagetThreeRelation, mother.tagetThreeRelation, this.calculateRelation(father.parent,mother.parent));
+          const relationTotal = targetOneRela + targetTwoRela + targetThreeRela;
+          relationArray.push({
+            targetOne: targetOne,
+            targetTwo: targetTwo,
+            targetThree: targetThree,
+            parentOne: father.parent,
+            parentTwo: mother.parent,
+            grandParentOne: father.grandFather,
+            grandParentTwo: father.grandMother,
+            grandParentThree: mother.grandFather,
+            grandParentFour: mother.grandMother,
+            targetOneRela: targetOneRela,
+            targetTwoRela: targetTwoRela,
+            targetThreeRela: targetThreeRela,
+            sum:  relationTotal,
+          })
+        }
+      })
+    })
+   
+
+    t1 = performance.now();
+    console.log("Parent time" + (t1 - t0) + " milliseconds.");
+    this.relationArray = relationArray;
+    /*
+    t0 = performance.now();
+    
+    const relatoinArray = relationArray.filter((hourseCompare) => (hourseCompare.targetOneRela >= this.state.targetOneMin
+      && hourseCompare.targetTwoRela >= this.state.targetTwoMin
+      && hourseCompare.targetThreeRela >= this.state.targetThreeMin));
+    const bestRelation :Horsecompare = _.sortBy(relatoinArray, [(horseRela) => -horseRela.sum])[0];
+    
+    t1 = performance.now();
+    console.log("filter time " + (t1 - t0) + " milliseconds.");
+    const bestTable = this.buildBestTable(bestRelation);
+    */
+
+    //return bestTable;
   }
 
   buildBestTable(relation: Horsecompare | undefined) {
@@ -341,23 +467,23 @@ class ReverseQuery extends Component<IProps, IState> {
   }
 
   render() {
+    
     const { localization } = this.props;
     let selectParentList : string[] = [];
     selectParentList.push('0000');
     selectParentList = selectParentList.concat(this.horses);
 
-    const relatoinArray = this.buildRelationArray().filter((hourseCompare) => (hourseCompare.targetOneRela >= this.state.targetOneMin
-              && hourseCompare.targetTwoRela >= this.state.targetTwoMin
-              && hourseCompare.targetThreeRela >= this.state.targetThreeMin));
+    
+    const relatoinArray = this.relationArray.filter((hourseCompare) => (hourseCompare.targetOneRela >= this.state.targetOneMin
+      && hourseCompare.targetTwoRela >= this.state.targetTwoMin
+      && hourseCompare.targetThreeRela >= this.state.targetThreeMin));
     const bestRelation :Horsecompare = _.sortBy(relatoinArray, [(horseRela) => -horseRela.sum])[0];
-    // eslint-disable-next-line no-console
-    console.log({ bestRelation });
+    
     const bestTable = this.buildBestTable(bestRelation);
+    
     return (
       <div className="content">
-        <div>
-          <span style={{ color: 'red' }}>Four unknown uma may crash</span>
-        </div>
+        
         <div className="dropdown">
           <FormControl>
             <InputLabel id="demo-simple-select-label">Target</InputLabel>
